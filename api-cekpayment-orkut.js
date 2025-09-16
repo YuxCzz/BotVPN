a365a1fc555f9b4121c9fc1af7d846b3e209b44c
ARI VPN STORES <145305412+arivpnstores@users.noreply.github.com>
2025-09-16 12:50:51 +0000

    Create api-cekpayment-orkut.js

---
+++ api-cekpayment-orkut.js
@@ -0,0 +1,32 @@
+// api-cekpayment-orkut.js
+const qs = require('qs');
+
+// bikin function biar gampang ganti request_time otomatis
+function buildPayload() {
+  return qs.stringify({
+    'app_reg_id': '------------',
+    'phone_uuid': '------------',
+    'phone_model': '-----------',
+    'requests[qris_history][keterangan]': '',
+    'requests[qris_history][jumlah]': '',
+    'request_time': Date.now().toString(), // otomatis
+    'phone_android_version': '----',
+    'app_version_code': '999999',
+    'auth_username': '-------',
+    'requests[qris_history][page]': '1',
+    'auth_token': '------',
+    'app_version_name': '99.99.99',
+    'ui_mode': 'dark'
+  });
+}
+
+const headers = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'Accept-Encoding': 'gzip',
+  'User-Agent': 'okhttp/4.12.0'
+};
+
+// URL sering berubah, jadi kamu bisa set default dulu
+const API_URL = 'https://app.orderkuota.com/api/v2/qris/mutasi/1xxxx';
+
+module.exports = { buildPayload, headers, API_URL };
